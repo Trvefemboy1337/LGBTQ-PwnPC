94a99ca8d2c914a28a5c7040fa6e0380c7c16e0b
Trvefemboy <trvefemboy@192.168.0.18>
2026-04-03 20:17:34 -0300

    Update some of WHOIS searching issues

--- mods/recon/whoislookup.py
+++ mods/recon/whoislookup.py
@@ -1,7 +1,19 @@
 import whois
+from colorama import init, Fore, Style
+
+init(autoreset=True)
 
 def whois_look():
     domain = input('Enter domain or IP to lookup: ')
     whois_result = whois.whois(domain)
-    print(f'Result for {domain} is {whois_result}')
+    print(Fore.GREEN + Style.BRIGHT + f'''Results for {domain} are
+        Domain: {whois_result.domain_name}
+        Registrar: {whois_result.registrar}
+        Created in: {whois_result.creation_date}
+        Expires in {whois_result}
+        Person: {whois_result.name}
+        Updated: {whois_result.updated_date}
+        Status: {whois_result.status}
+''')
+
 
